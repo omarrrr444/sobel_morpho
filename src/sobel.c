f63bae20203ccb681d66c0afd399b87b1a21f418
jeremy <jeremy.guillaume@centralesupelec.fr>
2023-12-12 14:23:58 +0100

    filtre_ff

--- src/sobel.c
+++ src/sobel.c
@@ -28,10 +28,13 @@ void sobel(int width, int height, unsigned char *input, unsigned char *output) {
                input[(j + 1) * width + i - 1] + 2 * input[(j + 1) * width + i] +
                input[(j + 1) * width + i + 1];
 
-      output[(j - 1) * width + i] = (gx * gx + gy * gy) / 4; // TODO
+      int gx = input[(j - 1) * width + i];
+      int gy = input[(j - 1) * width + i];	
+      output[(j-1)*width + i] = fminf(sqrtf(gx * gx + gy * gy), 255);
     }
   }
-
+  
+  
   // Fill the left and right sides
   for (j = 0; j < height - 2; j++) {
     output[j * width] = 0;
